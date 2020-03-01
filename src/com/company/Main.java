7fab919a57a53ba3f639565234789bbe6eca444e
AkshayRamesh002 <akshaypad@gmail.com>
2020-02-29 23:27:45 -0600

    Bad Commit - Logical Error

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code hereu
         System.out.println("Hello World Team City");
-        for(int i=0; i<5; i++){
+        for(int i=6; i>5; i++){
 
             if(i==4){
                 break;
