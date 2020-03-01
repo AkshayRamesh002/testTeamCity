272c21523795f3045f57131d8b6eeb4ee9c0fb14
AkshayRamesh002 <akshaypad@gmail.com>
2020-02-29 21:29:48 -0600

    Added For Loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code hereu
         System.out.println("Hello World Team City");
+        for(int i=0; i<5; i++){
+
+            System.out.println("Iteration : " + (i+1));
+        }
     }
 }
