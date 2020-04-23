a5fbd855b157120800dce8211efa177db3682c8e
akshayramesh002 <akshaypad@gmail.com>
2020-04-22 22:41:01 -0500

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code hereu
         System.out.println("Hello World Team City");
         System.out.println("Hello World Team City 2");
+        System.out.println(add(4,6));
 //        for(int i=6; i>5; i++){
 //
 //            if(i==4){
@@ -13,5 +14,9 @@ public class Main {
 //            }
 //            System.out.println("Iteration : " + (i+1));
 //        }
+    }
+
+    public static int add(int i, int j){
+        return i+j;
     }
 }
---
+++ test/com/company/MainTest.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        assertEquals(10,Main.add(6,4));
+    }
+}
